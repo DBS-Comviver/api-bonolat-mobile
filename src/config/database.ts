7c9a946263769e3cbf549a6d3196788e47040b63
DBS Easy Factory <dbs.easyfactory@asperbras.rede>
2025-11-19 15:39:45 -0300

    fix: package version

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma-client';
+import { PrismaClient } from '../generated/prisma-client/client';
 import { env } from './env';
 import { logger } from './logger';
 
